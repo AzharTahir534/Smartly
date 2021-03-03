a460487bce9cda373326bc75291ff115d4275795
Azhar <mtahir@sadadbahrain.com>
2021-03-03 20:51:05 +0500

    Initial Commit

---
+++ SmartlyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartlyApp
+//
+//  Created by Azhar on 03/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
